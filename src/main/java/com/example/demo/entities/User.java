3e73ff72c66b1e4c37ac7181b58020cd35090259
Julius <jzabulenas@protonmail.com>
2023-11-30 22:46:07 +0200

    Add Entity annotation

--- src/main/java/com/example/demo/entities/User.java
+++ src/main/java/com/example/demo/entities/User.java
@@ -1,10 +1,12 @@
 package com.example.demo.entities;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+@Entity
 @Table(name = "Users")
 public class User {
 
