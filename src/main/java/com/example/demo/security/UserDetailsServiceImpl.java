38c72320a78164edc6bfdd51c28ab7c4c7ce07cc
Julius <jzabulenas@protonmail.com>
2023-12-02 14:18:31 +0200

    Add @Service annotation

--- src/main/java/com/example/demo/security/UserDetailsServiceImpl.java
+++ src/main/java/com/example/demo/security/UserDetailsServiceImpl.java
@@ -3,10 +3,12 @@ package com.example.demo.security;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.example.demo.entities.User;
 import com.example.demo.repository.UserRepository;
 
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
 	private final UserRepository userRepository;
