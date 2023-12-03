8c8b9a81ee1dd5c408a2cffef293eeecfdc0996b
Julius <jzabulenas@protonmail.com>
2023-12-03 11:48:38 +0200

    Put Login component in route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
+import Login from "./components/Login";
 
 function App() {
   return (
@@ -7,6 +8,11 @@ function App() {
       <Route
         path="/"
         element={<Home />}
+      />
+
+      <Route
+        path="/login"
+        element={<Login />}
       />
     </Routes>
   );
