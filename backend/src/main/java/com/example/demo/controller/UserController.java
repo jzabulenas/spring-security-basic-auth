86c23a426a5a1ceb07b522fad55e1de9798ac77f
Julius <jzabulenas@protonmail.com>
2023-12-03 13:43:07 +0200

    Allow requests from frontend

--- backend/src/main/java/com/example/demo/controller/UserController.java
+++ backend/src/main/java/com/example/demo/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import com.example.demo.entities.User;
 import com.example.demo.repository.UserRepository;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 public class UserController {
 
 	private final UserRepository userRepository;
