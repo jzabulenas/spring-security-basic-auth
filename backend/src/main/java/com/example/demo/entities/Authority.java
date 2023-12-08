d2e082c956662e1b7a92212832e07cb38b297c21
Julius <jzabulenas@protonmail.com>
2023-12-08 23:53:02 +0200

    Remove Authority entity

--- backend/src/main/java/com/example/demo/entities/Authority.java
+++ backend/src/main/java/com/example/demo/entities/Authority.java
@@ -1,48 +1,48 @@
-package com.example.demo.entities;
-
-import java.util.Set;
-
-import org.springframework.security.core.GrantedAuthority;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.Table;
-
-@Entity
-@Table(name = "authorities")
-public class Authority implements GrantedAuthority {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
-
-	private String name;
-
-	@ManyToMany(mappedBy = "authorities")
-	private Set<User> users;
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Set<User> getUsers() {
-		return users;
-	}
-
-	public void setUsers(Set<User> users) {
-		this.users = users;
-	}
-
-	@Override
-	public String getAuthority() {
-		return name;
-	}
-
-}
+//package com.example.demo.entities;
+//
+//import java.util.Set;
+//
+//import org.springframework.security.core.GrantedAuthority;
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
+//import jakarta.persistence.ManyToMany;
+//import jakarta.persistence.Table;
+//
+//@Entity
+//@Table(name = "authorities")
+//public class Authority implements GrantedAuthority {
+//
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	private int id;
+//
+//	private String name;
+//
+//	@ManyToMany(mappedBy = "authorities")
+//	private Set<User> users;
+//
+//	public String getName() {
+//		return name;
+//	}
+//
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//
+//	public Set<User> getUsers() {
+//		return users;
+//	}
+//
+//	public void setUsers(Set<User> users) {
+//		this.users = users;
+//	}
+//
+//	@Override
+//	public String getAuthority() {
+//		return name;
+//	}
+//
+//}
